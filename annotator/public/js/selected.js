e1cea4f824afb3094a859640f0b020ef7c8d693a
Priansh Shah <shah@priansh.com>
2021-03-01 10:54:12 +0000

    rig rest of selected

--- annotator/public/js/selected.js
+++ annotator/public/js/selected.js
@@ -6,17 +6,22 @@ window.app = new Vue({
   data: {
     TAG: ['%c[MAIN]', 'background-color: #dd00aa; color: #000;'],
     hash: '',
-    datasetKey: '',
+    key: '',
+    name: '',
+    annotations: [],
+    activeAnnotation: -1,
   },
   async created() {
     console.time('APP STARTUP')
     info(...(this.TAG), 'Initializing application')
-    this.datasetKey = window.location.hash
+    this.key = window.location.hash
 
     //? setup IPC
     info(...(this.TAG), 'Initializing IPC')
     await this.initIPC()
 
+    //? load current dataset
+    this.loadDataset()
 
     success(...(this.TAG), 'Finished initialization.')
     console.timeEnd('APP STARTUP')
@@ -26,34 +31,34 @@ window.app = new Vue({
       console.log(...msg)
     },
     //? IPC Tasks
-    task_ListDatasets() {
-      return this.ipcTask('list datasets', {})
+    task_LoadDataset() {
+      return this.ipcTask('load dataset', { key: this.key })
     },
-    task_SaveDatasets() {
-      return this.ipcTask('save datasets', {
-        datasets: this.datasets
+    task_SaveDataset() {
+      return this.ipcTask('save dataset', {
+        key: this.key,
+        data: {
+          annotations: this.annotations,
+          name: this.name
+        }
       })
     },
     //? Dataset Management
-    async loadDatasets() {
-      this.datasets = await this.callIPC(this.task_ListDatasets())
+    async loadDataset() {
+      const { name, annotations } = await this.callIPC(this.task_LoadDataset())
+      this.name = name
+      this.annotations = annotations
     },
-    async addDataset() {
-      this.showAddDataset = false
-      const name = this.datasetName
-      const key = String.random(12)
-      this.datasets.unshift({ name, key })
-      this.datasets = await this.callIPC(this.task_SaveDatasets())
-      this.datasetName = ''
+    async saveDataset() {
+      console.log("saving dataset", this.annotations)
+      await this.callIPC(this.task_SaveDataset())
+      this.loadDataset()
+      this.activeAnnotation = -1
     },
-    async selectDataset(index) {
-      const dataset = this.datasets.splice(index, 1)?.[0]
-      //? move it to the top to indicate it is recently opened
-      this.datasets.unshift(dataset)
-      this.datasets = await this.callIPC(this.task_SaveDatasets())
-      // TODO: launch dataset editor
+    //? Annotation Management
+    async addAnnotation() {
+      this.annotations.unshift({ link: '', start: '', end: '', lyrics: '' })
+      this.activeAnnotation = 0
     }
   }
 })
-
-window.onresize = top.app.recalculateHeight
